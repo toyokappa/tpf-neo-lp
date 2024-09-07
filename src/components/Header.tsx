538490754aff48fe5a724fd85506207808101239
Yuta Toyokawa <kppg42@gmail.com>
2024-09-07 19:59:59 +0900

    ロゴの追加

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,23 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Header() {
   return (
     <header className="w-full py-3 border-b-4  border-sky-500">
-      <div className="md:flex items-center md:text-right text-center container mx-auto">
-        <Link href="/" className="block text-xl font-bold md:mb-0 mb-3">
-          (logo) 株式会社 TPF NEO
-        </Link>
-        <a
-          href="tel:0276-58-5123"
-          className="ms-auto px-4 py-2 bg-sky-500 text-white font-bold rounded-lg shadow"
+      <div className="md:flex items-center container mx-auto">
+        <Link
+          href="/"
+          className="flex items-center md:justify-normal justify-center text-xl"
         >
-          (tel): 0276-58-5123
-        </a>
+          <Image
+            src="/tpf-neo-logo.png"
+            alt="logo"
+            width={200}
+            height={50}
+            className="w-16 me-2"
+          />
+          <span>株式会社 TPF NEO</span>
+        </Link>
       </div>
     </header>
   );
